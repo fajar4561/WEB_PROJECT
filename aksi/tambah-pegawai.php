8cf70db43dfd3cd799d29c6991f038a9d9f86ce7
Saciku <maulanafajar751@gmail.com>
2021-11-27 17:32:06 +0700

    add auto generated kode pegawai
    Signed-off-by: Saciku <maulanafajar751@gmail.com>

--- aksi/tambah-pegawai.php
+++ aksi/tambah-pegawai.php
@@ -1,6 +1,15 @@
 <?php
 include 'dir.php';
 
+# Auto generated kode pegawai
+$quer = mysqli_query($koneksi, "SELECT max(kode_pegawai) as kodeTerbesar FROM pegawai ");
+$dat = mysqli_fetch_array($quer);
+$kodePegawai = $dat['kodeTerbesar'];
+$urutan = (int) substr($kodePegawai, 3, 3);
+$urutan++;
+$huruf='STF';
+$kodePegawai = $huruf. sprintf("%03s", $urutan);
+
 # Deklarasi Variable
 $nama = $_POST['nama'];
 $kelamin = $_POST['kelamin'];
@@ -16,6 +25,5 @@ $email = $_POST['email'];
 $telpon = $_POST['telpon'];
 $username = $_POST['username'];
 $password = $_POST['password'];
-
 
  ?>
