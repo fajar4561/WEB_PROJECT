f5a7e5bc600f35ab104b97f41be94788d33ca972
Saciku <maulanafajar751@gmail.com>
2021-11-27 17:17:45 +0700

    add komponen digunakan untuk menyimpan beberapa variable seperti : *Header *Navbar*Sidebar dll
    Signed-off-by: Saciku <maulanafajar751@gmail.com>

---
+++ komponen/bawah.php
@@ -0,0 +1,22 @@
+<!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="../vendors/popper/popper.min.js"></script>
+    <script src="../vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="../vendors/anchorjs/anchor.min.js"></script>
+    <script src="../vendors/is/is.min.js"></script>
+    <script src="../assets/js/flatpickr.js"></script>
+    <script src="../vendors/dropzone/dropzone.min.js"></script>
+    <script src="../vendors/lottie/lottie.min.js"></script>
+    <script src="../vendors/validator/validator.min.js"></script>
+    <script src="../vendors/prism/prism.js"></script>
+    <script src="../vendors/fontawesome/all.min.js"></script>
+    <script src="../vendors/lodash/lodash.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="../vendors/list.js/list.min.js"></script>
+    <script src="../assets/js/theme.js"></script>
+    <script src="../vendors/choices/choices.min.js"></script>
+
+  </body>
+
+</html>
