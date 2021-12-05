d27c41df0173c9443943c6824fc994e3a3ce25ab
Saciku <maulanafajar751@gmail.com>
2021-12-05 12:23:47 +0700

    pelanggan : checkout: Fix layout & add some form
    Signed-off-by: Saciku <maulanafajar751@gmail.com>

--- pelanggan/checkout.php
+++ pelanggan/checkout.php
@@ -3,6 +3,16 @@ $judul = 'App | checkout';
 include 'komponen/header.php';
 include '../koneksi.php';
 session_start();
+$today= date('Y-m-d');
+
+$penyebut = date('dmY');
+$quer = mysqli_query($koneksi, "SELECT max(kode_transaksi) as kodeTerbesar FROM beli ");
+$dat = mysqli_fetch_array($quer);
+$kodeTransaksi = $dat['kodeTerbesar'];
+$urutan = (int) substr($kodeTransaksi, 9, 3);
+$urutan++;
+$huruf='A';
+$kodeTransaksi = $huruf. $penyebut. sprintf("%03s", $urutan);
 ?>
   <body>
 
@@ -25,7 +35,7 @@ session_start();
             <div class="col-xl-4 order-xl-1">
               <div class="card">
                 <div class="card-header bg-light btn-reveal-trigger d-flex flex-between-center">
-                  <h5 class="mb-0">List Order</h5><a class="btn btn-link btn-sm btn-reveal text-600" href="../../app/e-commerce/shopping-cart.html"><span class="fas fa-pencil-alt"></span></a>
+                  <h5 class="mb-0">List Order <small class="text-primary"><?=$kodeTransaksi?></small></h5><a class="btn btn-link btn-sm btn-reveal text-600" href="../../app/e-commerce/shopping-cart.html"><span class="fas fa-pencil-alt"></span></a>
                 </div>
                 <div class="card-body">
                   <table class="table table-borderless fs--1 mb-0">
@@ -43,7 +53,7 @@ session_start();
                       <th class="ps-0 pt-0"><?=$pecah['nama_produk']?> x <?=$jml?>
                         <div class="text-400 fw-normal fs--2" style="white-space: pre-line;"><?=$pecah['deskripsi']?></div>
                       </th>
-                      <th class="pe-0 text-end pt-0"><?=$satuan?>K</th>
+                      <th class="pe-0 text-end pt-0 text-warning"><?=$satuan?>K</th>
                     </tr>
                     <?php $totalbelanja+=$subharga; ?>
                     <?php $jml_brg+=$jml?>
@@ -62,34 +72,72 @@ session_start();
                   <h5 class="mb-0">Form Pembeli</h5>
                 </div>
                 <div class="card-body">
-                  <form>
+                  <form class="needs-validation" novalidate="">
                     <div class="row gx-0 ps-2 mb-4">
                       <div class="col-sm-8 px-3">
+                        <div class="row mb-3">
+                          <div class="col-6">
+                            <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0" >Kode Transaksi</label>
+                            <input class="form-control" name="kode" type="text" placeholder="Kode Transaksi" value="<?=$kodeTransaksi?>"  readonly/>
+                          </div>
+                          <div class="col-6">
+                            <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0">Tanggal</label>
+                            <input class="form-control" name="tgl" type="date" value="<?=$today?>" />
+                          </div>
+                        </div>
                         <div class="mb-3">
                           <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0" for="inputNumber">Nama</label>
-                          <input class="form-control" name="nama" type="text" placeholder="Nama Pembeli"  />
+                          <input class="form-control" name="nama" type="text"  placeholder="Nama Pembeli" required="" />
+                          <div class="invalid-feedback mt-0">Pastikan Anda mengisi form nama.</div>
                         </div>
                         <div class="row align-items-center">
                           <div class="col-6">
                             <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0">No Telpon</label>
-                            <input class="form-control" type="text" placeholder="Whastapp / Telpon" />
+                            <input class="form-control" type="text" name="telpon" placeholder="Whastapp / Telpon" required />
+                            <div class="invalid-feedback mt-0">Silahkan isi no telephon.</div>
                           </div>
                           <div class="col-6">
                             <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0">Alamat<a class="d-inline-block" href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Alamat Pembeli"><span class="fa fa-question-circle ms-2"></span></a></label>
-                            <input class="form-control" type="text" placeholder="Alamat" />
+                            <input class="form-control" type="text" name="alamat"  placeholder="Alamat Pembeli" required />
+                            <div class="invalid-feedback mt-0">Pastikan Anda mengisi alamat.</div>
+                            <input type="hidden" name="total">
                           </div>
                         </div>
                         <div class="row align-items-center mt-4">
                           <div class="col-12">
-                            <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0">Rating</label>
-                            <select class="form-control">
-                              <option><span class="fa fa-star ms-2 text-warning">1</span></option>
-                            </select>
+                            <div class="rounded-1 p-2 bg-100">
+                              <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0 text-left">Rating<a class="d-inline-block" href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Silagkan Beri Rating Mengenai produk / layanan kami"><span class="fa fa-question-circle ms-2"></span></a></label><br>
+                              <small>
+                                <div class="form-check form-check-inline">
+                                  <input class="form-check-input" id="flexRadioDefault1" type="radio" name="rating" value="1" />
+                                  <label class="form-check-label" for="flexRadioDefault1">1<span class="fa fa-star ms-2 text-warning"></span></label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                  <input class="form-check-input" id="flexRadioDefault2" type="radio" name="rating" value="2" />
+                                  <label class="form-check-label" for="flexRadioDefault2">2<span class="fa fa-star ms-2 text-warning"></span></label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                  <input class="form-check-input" id="flexRadioDefault3" type="radio" name="rating" value="3"  />
+                                  <label class="form-check-label" for="flexRadioDefault3">3<span class="fa fa-star ms-2 text-warning"></span></label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                  <input class="form-check-input" id="flexRadioDefault4" type="radio" name="rating" value="4" />
+                                  <label class="form-check-label" for="flexRadioDefault4">4<span class="fa fa-star ms-2 text-warning"></span></label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                  <input class="form-check-input" id="flexRadioDefault5" type="radio" name="rating" value="5" checked="" />
+                                  <label class="form-check-label" for="flexRadioDefault5">5<span class="fa fa-star ms-2 text-warning"></span></label>
+                                </div>
+                              </small>
+                            </div>
                           </div>
                         </div>
                         <div class="row align-items-center mt-4">
-                          <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0">Komentar</label>
-                          <textarea class="form-control" id="basic-form-textarea" rows="3" placeholder="Description"></textarea>
+                          <div class="col-12">
+                            <label class="form-label ls text-uppercase text-600 fw-semi-bold mb-0">Komentar<a class="d-inline-block" href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Kritik dan saran anda mengenai produk / layanan kami"><span class="fa fa-question-circle ms-2"></span></a></label>
+                            <textarea class="form-control" id="basic-form-textarea" name="komentar" rows="3" placeholder="Description" required></textarea>
+                            <div class="invalid-feedback mt-0">Silahkan Isi Komentar.</div>
+                          </div>
                         </div>
                       </div>
                       <div class="col-4 ps-3 text-center pt-2 d-none d-sm-block">
@@ -103,22 +151,22 @@ session_start();
                       <div class="col-md-7 col-xl-12 col-xxl-7 px-md-3 mb-xxl-0 position-relative">
                         <div class="d-flex"><img class="me-3" src="../assets/img/icons/shield.png" alt="" width="60" height="60" />
                           <div class="flex-1">
-                            <h5 class="mb-2">Buyer Protection</h5>
+                            <h5 class="mb-2">Terms & Conditions</h5>
                             <div class="form-check mb-0">
-                              <input class="form-check-input" id="protection-option-1" type="checkbox" checked="checked" />
-                              <label class="form-check-label mb-0" for="protection-option-1"> <strong>Full Refund </strong>If you don't <br class="d-none d-md-block d-lg-none" />receive your order</label>
+                              <input class="form-check-input" id="protection-option-1" type="checkbox" required />
+                              <label class="form-check-label mb-0" for="protection-option-1"> <strong>Check Barang </strong>Pastikan Anda <br class="d-none d-md-block d-lg-none" />Mengechek barang yang anda beli</label>
                             </div>
                             <div class="form-check">
-                              <input class="form-check-input" id="protection-option-2" type="checkbox" checked="checked" />
-                              <label class="form-check-label mb-0" for="protection-option-2"> <strong>Full or Partial Refund, </strong>If the product is not as described in details</label>
-                            </div><a class="fs--1 ms-3 ps-2" href="#!">Learn More<span class="fas fa-caret-right ms-1" data-fa-transform="down-2">    </span></a>
+                              <input class="form-check-input" id="protection-option-2" type="checkbox" required />
+                              <label class="form-check-label mb-0" for="protection-option-2"> <strong>Hubungi Penjual, </strong>Apabila nanti barang tidak sesuai atau salah dalam proses pengiriman</label>
+                            </div><a class="fs--1 ms-3 ps-2" href="#!">Pelajari Selengkapnya<span class="fas fa-caret-right ms-1" data-fa-transform="down-2">    </span></a>
                           </div>
                         </div>
                         <div class="vertical-line d-none d-md-block d-xl-none d-xxl-block"> </div>
                       </div>
                       <div class="col-md-5 col-xl-12 col-xxl-5 ps-lg-4 ps-xl-2 ps-xxl-5 text-center text-md-start text-xl-center text-xxl-start">
                         <div class="border-dashed-bottom d-block d-md-none d-xl-block d-xxl-none my-4"></div>
-                        <div class="fs-2 fw-semi-bold">All Total: <span class="text-primary">$3320</span></div>
+                        <div class="fs-2 fw-semi-bold">Total: <span class="text-primary">Rp.<?=number_format($totalbelanja)?></span></div>
                         <button class="btn btn-success mt-3 px-5" type="submit">Confirm &amp; Pay</button>
                         <p class="fs--1 mt-3 mb-0">By clicking <strong>Confirm & Pay </strong>button you agree to the <a href="#!">Terms &amp; Conditions</a></p>
                       </div>
