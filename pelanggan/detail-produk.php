3f764c7254d74590008dd00178bcab55361c09c2
Saciku <maulanafajar751@gmail.com>
2021-12-05 17:58:58 +0700

    pelanggan:detail_produk: Add review produk pembeli
    Signed-off-by: Saciku <maulanafajar751@gmail.com>

--- pelanggan/detail-produk.php
+++ pelanggan/detail-produk.php
@@ -154,96 +154,84 @@ else {
                 <div class="col-12">
                   <div class="overflow-hidden mt-4">
                     <ul class="nav nav-tabs" id="myTab" role="tablist">
-                      <li class="nav-item"><a class="nav-link active ps-0" id="description-tab" data-bs-toggle="tab" href="#tab-description" role="tab" aria-controls="tab-description" aria-selected="true">Description</a></li>
-                      <li class="nav-item"><a class="nav-link px-2 px-md-3" id="specifications-tab" data-bs-toggle="tab" href="#tab-specifications" role="tab" aria-controls="tab-specifications" aria-selected="false">Specifications</a></li>
-                      <li class="nav-item"><a class="nav-link px-2 px-md-3" id="reviews-tab" data-bs-toggle="tab" href="#tab-reviews" role="tab" aria-controls="tab-reviews" aria-selected="false">Reviews</a></li>
+                      <li class="nav-item"><a class="nav-link active px-2 px-md-3" id="reviews-tab" data-bs-toggle="tab" href="#tab-reviews" role="tab" aria-controls="tab-reviews" aria-selected="false">Reviews</a></li>
                     </ul>
                     <div class="tab-content" id="myTabContent">
-                      <div class="tab-pane fade show active" id="tab-description" role="tabpanel" aria-labelledby="description-tab">
-                        <div class="mt-3">
-                          <p>Over the years, Apple has built a reputation for releasing its products with a lot of fanfare – but that didn’t exactly happen for the MacBook Pro 2018. Rather, Apple’s latest pro laptop experienced a subdued launch, in spite of it offering a notable spec upgrade over the 2017 model – along with an improved keyboard. And, as with previous generations the 15-inch MacBook Pro arrives alongside a 13-inch model.</p>
-                          <p>Apple still loves the MacBook Pro though, despite the quiet release. This is because, while the iPhone XS and iPad, along with the 12-inch MacBook, are aimed at everyday consumers, the MacBook Pro has always aimed at the creative and professional audience. This new MacBook Pro brings a level of performance (and price) unlike its more consumer-oriented devices. </p>
-                          <p>Still, Apple wants mainstream users to buy the MacBook Pro, too. So, if you’re just looking for the most powerful MacBook on the market, you’ll love this new MacBook Pro. Just keep in mind that, while the keyboard has been updated, there are still some issues with it.</p>
-                          <p>There’s enough of a difference between the two sizes when it comes to performance to warrant two separate reviews, and here we’ll be looking at how the flagship 15-inch MacBook Pro performs in 2019.</p>
-                          <p>It's build quality and design is batter than elit. Numquam excepturi a debitis, sint voluptates, nam odit vel delectus id repellendus vero reprehenderit quidem totam praesentium vitae nesciunt deserunt. Sint, veniam?</p>
-                        </div>
-                      </div>
-                      <div class="tab-pane fade" id="tab-specifications" role="tabpanel" aria-labelledby="specifications-tab">
-                        <table class="table fs--1 mt-3">
-                          <tbody>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Processor</td>
-                              <td>2.3GHz quad-core Intel Core i5,</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Memory</td>
-                              <td>8GB of 2133MHz LPDDR3 onboard memory</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Brand Name</td>
-                              <td>Apple</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Model</td>
-                              <td>Mac Book Pro</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Display</td>
-                              <td>13.3-inch (diagonal) LED-backlit display with IPS technology</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Storage</td>
-                              <td>512GB SSD</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Graphics</td>
-                              <td>Intel Iris Plus Graphics 655</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Weight</td>
-                              <td>7.15 pounds</td>
-                            </tr>
-                            <tr>
-                              <td class="bg-100" style="width: 30%;">Finish</td>
-                              <td>Silver, Space Gray</td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                      <div class="tab-pane fade" id="tab-reviews" role="tabpanel" aria-labelledby="reviews-tab">
+                      <div class="tab-pane show active fade" id="tab-reviews" role="tabpanel" aria-labelledby="reviews-tab">
                         <div class="row mt-3">
-                          <div class="col-lg-6 mb-4 mb-lg-0">
-                            <div class="mb-1"><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star text-warning fs--1"></span><span class="ms-3 text-dark fw-semi-bold">Awesome support, great code 😍</span>
+                          <div class="col-lg-12 mb-4 mb-lg-0">
+                            <?php
+                            $batas = 5;
+                            $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
+                            $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;  
+
+                            $previous = $halaman - 1;
+                            $next = $halaman + 1;
+
+                            $data = mysqli_query($koneksi,"select * from detail_beli INNER JOIN beli ON detail_beli.kode_transaksi=beli.kode_transaksi WHERE kode_produk='$kode'");
+                            $jumlah_data = mysqli_num_rows($data);
+                            $total_halaman = ceil($jumlah_data / $batas);
+
+                            $data_produk = mysqli_query($koneksi,"select * from detail_beli INNER JOIN beli ON detail_beli.kode_transaksi=beli.kode_transaksi WHERE kode_produk='$kode' limit $halaman_awal, $batas");
+                            $nomor = $halaman_awal+1;
+                            while($d = mysqli_fetch_array($data_produk)){
+                              ?>
+                              <?php
+                                // Tampilan Rating
+                              $rating2 = $d['rating'];
+                              if ($rating2 == 1 ) {
+                                $tampil_rating2 = '<span class="fa fa-star text-warning"></span>
+                                <span class="fa fa-star"></span>
+                                <span class="fa fa-star"></span>
+                                <span class="fa fa-star"></span>
+                                <span class="fa fa-star"></span>';
+                              }
+                              elseif ($rating2 == 2 ) {
+                               $tampil_rating2 = '<span class="fa fa-star text-warning"></span>
+                               <span class="fa fa-star text-warning"></span>
+                               <span class="fa fa-star"></span>
+                               <span class="fa fa-star"></span>
+                               <span class="fa fa-star"></span>'; 
+                             }
+                            elseif ($rating2 == 3 ) {
+                              $tampil_rating2 = '<span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star"></span>
+                              <span class="fa fa-star"></span>'; 
+                            }
+                            elseif ($rating2 == 4 ) {
+                              $tampil_rating2 = '<span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star"></span>'; 
+                            }
+                            else {
+                              $tampil_rating2 = '<span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>
+                              <span class="fa fa-star text-warning"></span>'; 
+                            }
+                               ?>
+                            <div class="mb-1"><?=$tampil_rating2?><span class="ms-3 text-dark fw-semi-bold text-lowercase"><?=$d['nama']?></span>
                             </div>
-                            <p class="fs--1 mb-2 text-600">By Drik Smith • October 14, 2019</p>
-                            <p class="mb-0">You shouldn't need to read a review to see how nice and polished this theme is. So I'll tell you something you won't find in the demo. After the download I had a technical question, emailed the team and got a response right from the team CEO with helpful advice.</p>
+                            <p class="fs--1 mb-2 text-600 text-lowercase">By <?=$d['nama']?> • <?=date('d F Y', strtotime($d['tgl_beli']));?></p>
+                            <p class="mb-0"><?=$d['komentar']?></p>
                             <hr class="my-4" />
-                            <div class="mb-1"><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star text-warning fs--1"></span><span class="fa fa-star-half-alt text-warning star-icon fs--1"></span><span class="ms-3 text-dark fw-semi-bold">Outstanding Design, Awesome Support</span>
-                            </div>
-                            <p class="fs--1 mb-2 text-600">By Liane • December 14, 2019</p>
-                            <p class="mb-0">This really is an amazing template - from the style to the font - clean layout. SO worth the money! The demo pages show off what Bootstrap 4 can impressively do. Great template!! Support response is FAST and the team is amazing - communication is important.</p>
-                          </div>
-                          <div class="col-lg-6 ps-lg-5">
-                            <form>
-                              <h5 class="mb-3">Write your Review</h5>
-                              <div class="mb-3">
-                                <label class="form-label">Ratting: </label>
-                                <div class="d-block" data-rater='{"starSize":32,"step":0.5}'></div>
-                              </div>
-                              <div class="mb-3">
-                                <label class="form-label" for="formGroupNameInput">Name:</label>
-                                <input class="form-control" id="formGroupNameInput" type="text" />
-                              </div>
-                              <div class="mb-3">
-                                <label class="form-label" for="formGroupEmailInput">Email:</label>
-                                <input class="form-control" id="formGroupEmailInput" type="email" />
-                              </div>
-                              <div class="mb-3">
-                                <label class="form-label" for="formGrouptextareaInput">Review:</label>
-                                <textarea class="form-control" id="formGrouptextareaInput" rows="3"></textarea>
-                              </div>
-                              <button class="btn btn-primary" type="submit">Submit</button>
-                            </form>
+                            <?php } ?>
+                            <center>
+                              <?php
+                                if ($halaman < $total_halaman) {
+                                  echo "<a href='?halaman=$next&kode=$kode'>Berikutnya</a>";
+                                }
+                                if ($halaman > 1) {
+                                  echo "<a href='?halaman=$previous&kode=$kode'>Sebelumnya</a>";
+                                }
+                               ?>
+                              
+                            </center>
                           </div>
                         </div>
                       </div>
