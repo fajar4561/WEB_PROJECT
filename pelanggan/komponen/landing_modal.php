67f907ae11cb3511ef278c6af7b4435fa3b22142
Saciku <maulanafajar751@gmail.com>
2021-12-06 17:56:18 +0700

    pelanggan: Add Landing Page

---
+++ pelanggan/komponen/landing_modal.php
@@ -0,0 +1,47 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-body p-4">
+              <div class="row text-start justify-content-between align-items-center mb-2">
+                <div class="col-auto">
+                  <h5 id="modalLabel">Register</h5>
+                </div>
+                <div class="col-auto">
+                  <p class="fs--1 text-600 mb-0">Have an account? <a href="../pages/authentication/simple/login.html">Login</a></p>
+                </div>
+              </div>
+              <form>
+                <div class="mb-3">
+                  <input class="form-control" type="text" autocomplete="on" placeholder="Name" />
+                </div>
+                <div class="mb-3">
+                  <input class="form-control" type="email" autocomplete="on" placeholder="Email address" />
+                </div>
+                <div class="row gx-2">
+                  <div class="mb-3 col-sm-6">
+                    <input class="form-control" type="password" autocomplete="on" placeholder="Password" />
+                  </div>
+                  <div class="mb-3 col-sm-6">
+                    <input class="form-control" type="password" autocomplete="on" placeholder="Confirm Password" />
+                  </div>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="modal-register-checkbox" />
+                  <label class="form-label" for="modal-register-checkbox">I accept the <a href="#!">terms </a>and <a href="#!">privacy policy</a></label>
+                </div>
+                <div class="mb-3">
+                  <button class="btn btn-primary d-block w-100 mt-3" type="submit" name="submit">Register</button>
+                </div>
+              </form>
+              <div class="position-relative mt-4">
+                <hr class="bg-300" />
+                <div class="divider-content-center">or register with</div>
+              </div>
+              <div class="row g-2 mt-2">
+                <div class="col-sm-6"><a class="btn btn-outline-google-plus btn-sm d-block w-100" href="#"><span class="fab fa-google-plus-g me-2" data-fa-transform="grow-8"></span> google</a></div>
+                <div class="col-sm-6"><a class="btn btn-outline-facebook btn-sm d-block w-100" href="#"><span class="fab fa-facebook-square me-2" data-fa-transform="grow-8"></span> facebook</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
