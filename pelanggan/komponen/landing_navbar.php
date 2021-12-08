88aab7358d518d5ef51d7a31d1d03e5db7880142
Saciku <maulanafajar751@gmail.com>
2021-12-08 13:20:53 +0700

    pelanggan: Index: Update some changes
    Signed-off-by: Saciku <maulanafajar751@gmail.com>

--- pelanggan/komponen/landing_navbar.php
+++ pelanggan/komponen/landing_navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-standard navbar-expand-lg fixed-top navbar-dark" data-navbar-darken-on-scroll="data-navbar-darken-on-scroll">
-        <div class="container"><a class="navbar-brand" href="../index.html"><span class="text-white dark__text-white">Falcon</span></a>
+        <div class="container"><a class="navbar-brand" href="../index.html"><span class="text-white dark__text-white">Shey Sports</span></a>
           <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarStandard" aria-controls="navbarStandard" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse scrollbar" id="navbarStandard">
             <ul class="navbar-nav" data-top-nav-dropdowns="data-top-nav-dropdowns">
