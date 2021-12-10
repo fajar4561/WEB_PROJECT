363dca43df903834a1b266e1e9285fd3550380b5
Saciku <maulanafajar751@gmail.com>
2021-12-10 09:17:59 +0700

    pelanggan: Add Notifikasi count keranjang belanja
    Signed-off-by: Saciku <maulanafajar751@gmail.com>

--- pelanggan/komponen/topbar.php
+++ pelanggan/komponen/topbar.php
@@ -2,7 +2,7 @@
 
             <button class="btn navbar-toggler-humburger-icon navbar-toggler me-1 me-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarVerticalCollapse" aria-controls="navbarVerticalCollapse" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
             <a class="navbar-brand me-1 me-sm-3" href="../index.html">
-              <div class="d-flex align-items-center"><img class="me-2" src="../assets/img/icons/spot-illustrations/falcon.png" alt="" width="40" /><span class="font-sans-serif">falcon</span>
+              <div class="d-flex align-items-center"><img class="me-2" src="../assets/img/icons/spot-illustrations/falcon.png" alt="" width="40" /><span class="font-sans-serif">Shey Sports</span>
               </div>
             </a>
             <ul class="navbar-nav align-items-center d-none d-lg-block">
@@ -84,132 +84,69 @@
                   <label class="mb-0 theme-control-toggle-label theme-control-toggle-dark" for="themeControlToggle" data-bs-toggle="tooltip" data-bs-placement="left" title="Switch to dark theme"><span class="fas fa-moon fs-0"></span></label>
                 </div>
               </li>
-              <li class="nav-item">
-                <a class="nav-link px-0 notification-indicator notification-indicator-warning notification-indicator-fill fa-icon-wait" href="../app/e-commerce/shopping-cart.html"><span class="fas fa-shopping-cart" data-fa-transform="shrink-7" style="font-size: 33px;"></span><span class="notification-indicator-number">1</span></a>
-
-              </li>
+              <!-- Notifikasi keranjang belanja -->
+                <?php $nomor=0; ?>
+                <?php foreach ($_SESSION["keranjang"] as $id => $jml): ?>
+                <?php $nomor += 1; ?>
+                <?php endforeach; ?>
+                <?php
+                  if (empty($_SESSION["keranjang"]) OR !isset($_SESSION["keranjang"])) {
+                    $tampil_count_notif= '<span class="fas fa-shopping-cart" data-fa-transform="shrink-6" style="font-size: 33px;"></span>';
+                    $style_notif ='';
+                    $pesan_notif ='Keranjang Masih kosong';
+                    $link_keranjang ='';
+                  }
+                  else {
+                    $tampil_count_notif= '<span class="fas fa-shopping-cart" data-fa-transform="shrink-6" style="font-size: 33px;"></span><span class="notification-indicator-number">'.$nomor.'</span>';
+                    $style_notif ='notification-indicator notification-indicator-primary px-0 fa-icon-wait';
+                    $pesan_notif ='New';
+                    $link_keranjang = '<div class="card-footer text-center border-top"><a class="card-link d-block" href="keranjang">View all</a></div>';
+                  }
+                 ?>
               <li class="nav-item dropdown">
-                <a class="nav-link notification-indicator notification-indicator-primary px-0 fa-icon-wait" id="navbarDropdownNotification" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fas fa-bell" data-fa-transform="shrink-6" style="font-size: 33px;"></span></a>
+                <a class="nav-link <?=$style_notif?>" id="navbarDropdownNotification" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?=$tampil_count_notif?></a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-card dropdown-menu-notification" aria-labelledby="navbarDropdownNotification">
                   <div class="card card-notification shadow-none">
                     <div class="card-header">
                       <div class="row justify-content-between align-items-center">
                         <div class="col-auto">
-                          <h6 class="card-header-title mb-0">Notifications</h6>
+                          <h6 class="card-header-title mb-0">Notifikasi</h6>
                         </div>
-                        <div class="col-auto ps-0 ps-sm-3"><a class="card-link fw-normal" href="#">Mark all as read</a></div>
+                        <div class="col-auto ps-0 ps-sm-3"><a class="card-link fw-normal" href="#"></a></div>
                       </div>
                     </div>
                     <div class="scrollbar-overlay" style="max-height:19rem">
                       <div class="list-group list-group-flush fw-normal fs--1">
-                        <div class="list-group-title border-bottom">NEW</div>
+                        <div class="list-group-title border-bottom"><?=$pesan_notif?></div>
+
+                        <?php foreach ($_SESSION["keranjang"] as $id => $jml): ?>
+                          <?php
+                          $ambil_notif = $koneksi->query("SELECT * FROM produk WHERE id='$id'");
+                          $data_notif = $ambil_notif->fetch_assoc();
+                        ?>
                         <div class="list-group-item">
                           <a class="notification notification-flush notification-unread" href="#!">
                             <div class="notification-avatar">
                               <div class="avatar avatar-2xl me-3">
-                                <img class="rounded-circle" src="../assets/img/team/1-thumb.png" alt="" />
+                                <img class="rounded-circle" src="../fotoproduk/<?=$data_notif['foto']?>" alt="" />
 
                               </div>
                             </div>
                             <div class="notification-body">
-                              <p class="mb-1"><strong>Emma Watson</strong> replied to your comment : "Hello world 😍"</p>
-                              <span class="notification-time"><span class="me-2" role="img" aria-label="Emoji">💬</span>Just now</span>
+                              <p class="mb-1"><strong><?=implode(" ", array_slice(explode(" ", $data_notif['nama_produk']), 0, 2));?></strong> <?=substr($data_notif["deskripsi"],0,100).'.....'?>"</p>
+                              <span class="notification-time"><span class="me-2" role="img" aria-label="Emoji"><?=$jml?> x</span>Rp.<?=number_format($data_notif['harga'])?></span>
 
                             </div>
                           </a>
 
                         </div>
-                        <div class="list-group-item">
-                          <a class="notification notification-flush notification-unread" href="#!">
-                            <div class="notification-avatar">
-                              <div class="avatar avatar-2xl me-3">
-                                <div class="avatar-name rounded-circle"><span>AB</span></div>
-                              </div>
-                            </div>
-                            <div class="notification-body">
-                              <p class="mb-1"><strong>Albert Brooks</strong> reacted to <strong>Mia Khalifa's</strong> status</p>
-                              <span class="notification-time"><span class="me-2 fab fa-gratipay text-danger"></span>9hr</span>
-
-                            </div>
-                          </a>
-
-                        </div>
-                        <div class="list-group-title border-bottom">EARLIER</div>
-                        <div class="list-group-item">
-                          <a class="notification notification-flush" href="#!">
-                            <div class="notification-avatar">
-                              <div class="avatar avatar-2xl me-3">
-                                <img class="rounded-circle" src="../assets/img/icons/weather-sm.jpg" alt="" />
-
-                              </div>
-                            </div>
-                            <div class="notification-body">
-                              <p class="mb-1">The forecast today shows a low of 20&#8451; in California. See today's weather.</p>
-                              <span class="notification-time"><span class="me-2" role="img" aria-label="Emoji">🌤️</span>1d</span>
-
-                            </div>
-                          </a>
-
-                        </div>
-                        <div class="list-group-item">
-                          <a class="border-bottom-0 notification-unread  notification notification-flush" href="#!">
-                            <div class="notification-avatar">
-                              <div class="avatar avatar-xl me-3">
-                                <img class="rounded-circle" src="../assets/img/logos/oxford.png" alt="" />
-
-                              </div>
-                            </div>
-                            <div class="notification-body">
-                              <p class="mb-1"><strong>University of Oxford</strong> created an event : "Causal Inference Hilary 2019"</p>
-                              <span class="notification-time"><span class="me-2" role="img" aria-label="Emoji">✌️</span>1w</span>
-
-                            </div>
-                          </a>
-
-                        </div>
-                        <div class="list-group-item">
-                          <a class="border-bottom-0 notification notification-flush" href="#!">
-                            <div class="notification-avatar">
-                              <div class="avatar avatar-xl me-3">
-                                <img class="rounded-circle" src="../assets/img/team/10.jpg" alt="" />
-
-                              </div>
-                            </div>
-                            <div class="notification-body">
-                              <p class="mb-1"><strong>James Cameron</strong> invited to join the group: United Nations International Children's Fund</p>
-                              <span class="notification-time"><span class="me-2" role="img" aria-label="Emoji">🙋‍</span>2d</span>
-
-                            </div>
-                          </a>
-
-                        </div>
+                        <?php endforeach; ?>
                       </div>
                     </div>
-                    <div class="card-footer text-center border-top"><a class="card-link d-block" href="../app/social/notifications.html">View all</a></div>
+                    <?=$link_keranjang?>
                   </div>
                 </div>
 
-              </li>
-              <li class="nav-item dropdown"><a class="nav-link pe-0" id="navbarDropdownUser" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <div class="avatar avatar-xl">
-                    <img class="rounded-circle" src="../assets/img/team/3-thumb.png" alt="" />
-
-                  </div>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end py-0" aria-labelledby="navbarDropdownUser">
-                  <div class="bg-white dark__bg-1000 rounded-2 py-2">
-                    <a class="dropdown-item fw-bold text-warning" href="#!"><span class="fas fa-crown me-1"></span><span>Go Pro</span></a>
-
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#!">Set status</a>
-                    <a class="dropdown-item" href="../pages/user/profile.html">Profile &amp; account</a>
-                    <a class="dropdown-item" href="#!">Feedback</a>
-
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="../pages/user/settings.html">Settings</a>
-                    <a class="dropdown-item" href="../pages/authentication/card/logout.html">Logout</a>
-                  </div>
-                </div>
               </li>
             </ul>
           </nav>
