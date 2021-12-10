0e64418a0f04c09da7aebf915f317aa4a0566059
Saciku <maulanafajar751@gmail.com>
2021-12-10 16:40:59 +0700

    pelanggan: update customize theme control
    Signed-off-by: Saciku <maulanafajar751@gmail.com>

--- pelanggan/komponen/kustom.php
+++ pelanggan/komponen/kustom.php
@@ -8,17 +8,17 @@
         <button class="btn-close btn-close-white z-index-1 mt-0" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body scrollbar-overlay px-card" id="themeController">
-        <h5 class="fs-0">Color Scheme</h5>
-        <p class="fs--1">Choose the perfect color mode for your app.</p>
+        <h5 class="fs-0">Thema Web</h5>
+        <p class="fs--1">silahkan pilih tampilan sesuai dengan selera anda.</p>
         <div class="btn-group d-block w-100 btn-group-navbar-style">
           <div class="row gx-2">
             <div class="col-6">
               <input class="btn-check" id="themeSwitcherLight" name="theme-color" type="radio" value="light" data-theme-control="theme" />
-              <label class="btn d-inline-block btn-navbar-style fs--1" for="themeSwitcherLight"> <span class="hover-overlay mb-2 rounded d-block"><img class="img-fluid img-prototype mb-0" src="../assets/img/generic/falcon-mode-default.jpg" alt=""/></span><span class="label-text">Light</span></label>
+              <label class="btn d-inline-block btn-navbar-style fs--1" for="themeSwitcherLight"> <span class="hover-overlay mb-2 rounded d-block"><img class="img-fluid img-prototype mb-0" src="../assets/img/generic/falcon-mode-default.jpg" alt=""/></span><span class="label-text">Terang</span></label>
             </div>
             <div class="col-6">
               <input class="btn-check" id="themeSwitcherDark" name="theme-color" type="radio" value="dark" data-theme-control="theme" />
-              <label class="btn d-inline-block btn-navbar-style fs--1" for="themeSwitcherDark"> <span class="hover-overlay mb-2 rounded d-block"><img class="img-fluid img-prototype mb-0" src="../assets/img/generic/falcon-mode-dark.jpg" alt=""/></span><span class="label-text"> Dark</span></label>
+              <label class="btn d-inline-block btn-navbar-style fs--1" for="themeSwitcherDark"> <span class="hover-overlay mb-2 rounded d-block"><img class="img-fluid img-prototype mb-0" src="../assets/img/generic/falcon-mode-dark.jpg" alt=""/></span><span class="label-text"> Gelap</span></label>
             </div>
           </div>
         </div>
@@ -27,7 +27,7 @@
           <div class="d-flex align-items-start"><img class="me-2" src="../assets/img/icons/left-arrow-from-left.svg" width="20" alt="" />
             <div class="flex-1">
               <h5 class="fs-0">RTL Mode</h5>
-              <p class="fs--1 mb-0">Switch your language direction </p><a class="fs--1" href="../documentation/customization/configuration.html">RTL Documentation</a>
+              <p class="fs--1 mb-0">Tampilan Rata Kanan</p><a class="fs--1" href="#!">RTL Documentation</a>
             </div>
           </div>
           <div class="form-check form-switch">
@@ -39,7 +39,7 @@
           <div class="d-flex align-items-start"><img class="me-2" src="../assets/img/icons/arrows-h.svg" width="20" alt="" />
             <div class="flex-1">
               <h5 class="fs-0">Fluid Layout</h5>
-              <p class="fs--1 mb-0">Toggle container layout system </p><a class="fs--1" href="../documentation/customization/configuration.html">Fluid Documentation</a>
+              <p class="fs--1 mb-0">Tampilan halaman dalam ukuran layar penuh </p><a class="fs--1" href="#!">Fluid Documentation</a>
             </div>
           </div>
           <div class="form-check form-switch">
@@ -47,53 +47,9 @@
           </div>
         </div>
         <hr />
-        <div class="d-flex align-items-start"><img class="me-2" src="../assets/img/icons/paragraph.svg" width="20" alt="" />
-          <div class="flex-1">
-            <h5 class="fs-0 d-flex align-items-center">Navigation Position </h5>
-            <p class="fs--1 mb-2">Select a suitable navigation system for your web application </p>
-            <div>
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" id="option-navbar-vertical" type="radio" name="navbar" value="vertical" data-page-url="../modules/components/navs-and-tabs/vertical-navbar.html" data-theme-control="navbarPosition" />
-                <label class="form-check-label" for="option-navbar-vertical">Vertical</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" id="option-navbar-top" type="radio" name="navbar" value="top" data-page-url="../modules/components/navs-and-tabs/top-navbar.html" data-theme-control="navbarPosition" />
-                <label class="form-check-label" for="option-navbar-top">Top</label>
-              </div>
-              <div class="form-check form-check-inline me-0">
-                <input class="form-check-input" id="option-navbar-combo" type="radio" name="navbar" value="combo" data-page-url="../modules/components/navs-and-tabs/combo-navbar.html" data-theme-control="navbarPosition" />
-                <label class="form-check-label" for="option-navbar-combo">Combo</label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr />
-        <h5 class="fs-0 d-flex align-items-center">Vertical Navbar Style</h5>
-        <p class="fs--1 mb-0">Switch between styles for your vertical navbar </p>
-        <p> <a class="fs--1" href="../modules/components/navs-and-tabs/vertical-navbar.html#navbar-styles">See Documentation</a></p>
-        <div class="btn-group d-block w-100 btn-group-navbar-style">
-          <div class="row gx-2">
-            <div class="col-6">
-              <input class="btn-check" id="navbar-style-transparent" type="radio" name="navbarStyle" value="transparent" data-theme-control="navbarStyle" />
-              <label class="btn d-block w-100 btn-navbar-style fs--1" for="navbar-style-transparent"> <img class="img-fluid img-prototype" src="../assets/img/generic/default.png" alt="" /><span class="label-text"> Transparent</span></label>
-            </div>
-            <div class="col-6">
-              <input class="btn-check" id="navbar-style-inverted" type="radio" name="navbarStyle" value="inverted" data-theme-control="navbarStyle" />
-              <label class="btn d-block w-100 btn-navbar-style fs--1" for="navbar-style-inverted"> <img class="img-fluid img-prototype" src="../assets/img/generic/inverted.png" alt="" /><span class="label-text"> Inverted</span></label>
-            </div>
-            <div class="col-6">
-              <input class="btn-check" id="navbar-style-card" type="radio" name="navbarStyle" value="card" data-theme-control="navbarStyle" />
-              <label class="btn d-block w-100 btn-navbar-style fs--1" for="navbar-style-card"> <img class="img-fluid img-prototype" src="../assets/img/generic/card.png" alt="" /><span class="label-text"> Card</span></label>
-            </div>
-            <div class="col-6">
-              <input class="btn-check" id="navbar-style-vibrant" type="radio" name="navbarStyle" value="vibrant" data-theme-control="navbarStyle" />
-              <label class="btn d-block w-100 btn-navbar-style fs--1" for="navbar-style-vibrant"> <img class="img-fluid img-prototype" src="../assets/img/generic/vibrant.png" alt="" /><span class="label-text"> Vibrant</span></label>
-            </div>
-          </div>
-        </div>
         <div class="text-center mt-5"><img class="mb-4" src="../assets/img/icons/spot-illustrations/47.png" alt="" width="120" />
-          <h5>Like What You See?</h5>
-          <p class="fs--1">Get Falcon now and create beautiful dashboards with hundreds of widgets.</p><a class="mb-3 btn btn-primary" href="https://themes.getbootstrap.com/product/falcon-admin-dashboard-webapp-template/" target="_blank">Purchase</a>
+          <h5>Anda mengalami kesulitan?</h5>
+          <p class="fs--1">Silahkan hubungi kami apabila anda mengalami kesulitan dalam berbelanja di situs kami.</p><a class="mb-3 btn btn-primary" href="#!" >Hubungi Kami</a>
         </div>
       </div>
     </div><a class="card setting-toggle" href="#settings-offcanvas" data-bs-toggle="offcanvas">
